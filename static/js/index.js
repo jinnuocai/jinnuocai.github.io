b69358dfc65874613281c7ea58a2ec77091637bb
jinnuocai <37980963+jinnuocai@users.noreply.github.com>
2018-12-11 20:37:09 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 function openZoosUrl()
 {
-    window.open("http://wpa.qq.com/msgrd?v=3&uin=421666621&site=qq&menu=yes");    
+    window.open("http://wpa.qq.com/msgrd?v=3&uin=421353409&site=qq&menu=yes");    
 }
 
 $(function(){
